c010832bfe188c48a32f54dedfa4da29c483a5ff
Drathonix <52723768+Drathonix@users.noreply.github.com>
2025-11-14 15:51:10 -0500

    Moved basically all the custom stuff to a buildSrc plugin. Made pluralization actually support the "resources.pluralization.targets" field

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import dev.kikugie.stonecutter.settings.StonecutterSettings
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -18,42 +16,38 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.6-beta.1"
+    id("dev.kikugie.stonecutter") version "0.7.10"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-extensions.configure<StonecutterSettings> {
+
+stonecutter {
     kotlinController = true
     centralScript = "build.gradle.kts"
 
     // TODO Disable any versions you don't want to support anb enable any versions you want to support
     // The versions listed here, commented out or otherwise, all have pre-made gradle.properties.
     shared {
-        /*vers("1.16.5-fabric","1.16.5")
-        vers("1.16.5-forge","1.16.5")
-        vers("1.17.1-fabric","1.17.1")
-        vers("1.17.1-forge","1.17.1")*/
-        /*vers("1.18.2-fabric","1.18.2")
-        vers("1.18.2-forge","1.18.2")
-        vers("1.19.2-fabric","1.19.2")
-        vers("1.19.2-forge","1.19.2")
-        vers("1.19.4-fabric","1.19.4")
-        vers("1.19.4-forge","1.19.4")
-        vers("1.20.1-fabric","1.20.1")
-        vers("1.20.1-forge","1.20.1")*/
-        /*vers("1.20.2-fabric","1.20.2")
-        vers("1.20.2-forge","1.20.2")
-        vers("1.20.2-neoforge","1.20.2")*/
-        vers("1.20.4-fabric","1.20.4")
-        vers("1.20.4-forge","1.20.4")
-        vers("1.20.4-neoforge","1.20.4")
-        /*vers("1.20.6-fabric","1.20.6")
-        vers("1.20.6-neoforge","1.20.6")
-        vers("1.21-fabric","1.21")
-        vers("1.21-neoforge","1.21")
-        vers("1.21.1-fabric","1.21.1")
-        vers("1.21.1-neoforge","1.21.1")
-        vers("1.21.2+3-fabric","1.21.2")
-        vers("1.21.2+3-neoforge","1.21.2")*/
+        /*version("1.16.5-fabric","1.16.5")
+        version("1.16.5-forge","1.16.5")
+        version("1.18.2-fabric","1.18.2")
+        version("1.18.2-forge","1.18.2")
+        version("1.19.2-fabric","1.19.2")
+        version("1.19.2-forge","1.19.2")
+        version("1.19.4-fabric","1.19.4")
+        version("1.19.4-forge","1.19.4")
+        version("1.20.1-fabric","1.20.1")
+        version("1.20.1-forge","1.20.1")*/
+        version("1.20.4-fabric","1.20.4")
+        version("1.20.4-forge","1.20.4")
+        version("1.20.4-neoforge","1.20.4")
+        /*version("1.20.6-fabric","1.20.6")
+        version("1.20.6-neoforge","1.20.6")
+        version("1.21-fabric","1.21")
+        version("1.21-neoforge","1.21")
+        version("1.21.1-fabric","1.21.1")
+        version("1.21.1-neoforge","1.21.1")
+        version("1.21.2+3-fabric","1.21.2")
+        version("1.21.2+3-neoforge","1.21.2")*/
         vcsVersion="1.20.4-fabric"
     }
     create(rootProject)
