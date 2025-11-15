3ff9e5e7d7e8fda6f376668e66042d3fe8e4f070
Drathonix <52723768+Drathonix@users.noreply.github.com>
2025-11-14 19:04:59 -0500

    Fix affore-mentioned mistaken replacement.

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("dev.kikugie.j52j") version "1.0" apply false // Enables asset processing by writing json5 files
     id("me.modmuss50.mod-publish-plugin") version "0.5.+" apply false // Publishes builds to hosting websites
     id("architectury-plugin") version "3.4-SNAPSHOT" apply false
-    id("templatemodid") apply false
+    id("rockbreaker") apply false
 }
 
 stonecutter active "1.20.4-fabric" /* You may have to edit this. Make sure it matches one of the versions present in settings.gradle.kts */
